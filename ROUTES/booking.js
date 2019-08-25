763550c45ceb81ebab724243da317a309018bd75
bibash44 <bibashkatel4@gmail.com>
2019-08-25 14:28:01 +0545

    fixed

--- ROUTES/booking.js
+++ ROUTES/booking.js
@@ -29,9 +29,6 @@ var fullTime = hour + ':' + minutes + ':' + seconds + ' ' + am_pm;
 
 router.post('/', function(req, res) {
 
-    console.log('request received')
-    console.log(req.body)
-
     var date = fullDate + ' at ' + fullTime;
     var username = req.body.name;
     var useremail = req.body.email;
@@ -62,86 +59,18 @@ router.post('/', function(req, res) {
 
 
 router.get('/', function(req, res) {
-    var select_booking = "SELECT * FROM booking ORDER BY date DESC";
+    var select_booking = "SELECT * FROM booking ORDER BY booked_time DESC";
 
     db.query(select_booking, function(err, result, fields) {
-        if (err) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({
-                database_failed: true
-
-            }, null, 3));
-        } else if (result.length > 0) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({
-                success: true,
-                data: result
-
-            }, null, 3));
-        } else {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({
-                success: false,
-                data: result
-
-            }, null, 3));
-        }
-
+        if (err) throw err;
+        res.json(result)
     });
 })
 
 
-// uploadImage
-app.use("/uploads/images/booking", express.static("upload/images/booking"))
-
-var ImagefileName = '';
-var storage = multer.diskStorage({
-    destination: 'uploads/images/booking',
-    filename: function(req, file, callback) {
-        const extension = path.extname(file.originalname);
-        ImagefileName = file.fieldname + Date.now() + extension;
-        callback(null, ImagefileName);
-
-    }
-});
-
-
-var imageFileFilter = (req, file, cb) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return cb(new Error("You can upload only image files!"), false);
-    }
-    cb(null, true);
-};
-
-var upload = multer({
-    storage: storage,
-    fileFilter: imageFileFilter,
-    limits: {
-        fileSize: 10000000
-    }
-});
-
-router.post('/upload/image', upload.single('image'), function(req, res) {
-    console.log(ImagefileName)
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        image: ImagefileName
-    }, null, 3));
-})
-
 router.delete('/', (req, res) => {
     var id = req.body.id;
-    var imageName = req.body.image;
-    console.log(req.body)
-    console.log(imageName)
-    fs.unlink('./UPLOADS/images/booking/' + imageName, function(err) {
-        if (err) {
-            return console.log(err)
-        } else {
-            console.log('old file unlinked')
-        }
-    })
-
+    
     var deletebooking = 'DELETE FROM booking WHERE id="' + id + '"';
     db.query(deletebooking, function(err, result, fields) {
         if (err) throw err;
